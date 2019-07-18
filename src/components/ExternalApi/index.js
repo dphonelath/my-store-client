8c13f16173a9ec5ce74d16e5374f944717859afd
Daniel Phonelath <daniel.phonelath@gmail.com>
2019-07-18 16:08:35 -0600

    TestApi is working and added PrivateRoutes

--- src/components/ExternalApi/index.js
+++ src/components/ExternalApi/index.js
@@ -10,7 +10,7 @@ const ExternalApi = () => {
     try {
       const token = await getTokenSilently()
 
-      const response = await fetch("/api/external", {
+      const response = await fetch("http://localhost:4000/api/external", {
         headers: {
           Authorization: `Bearer ${token}`
         }
