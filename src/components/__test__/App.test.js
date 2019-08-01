f891430762c3cc1ca3df85d89af6745139de5f71
Daniel Phonelath <daniel.phonelath@gmail.com>
2019-08-01 14:55:22 -0600

    added tests for APP and NAV

--- src/components/__test__/App.test.js
+++ src/components/__test__/App.test.js
@@ -1,2 +1,18 @@
 import React from "react"
 import App from "../../App"
+import { shallow } from "enzyme"
+
+import { Route } from "react-dom"
+
+it("my first test", () => {
+  const isTrue = true
+  expect(isTrue).toBe(true)
+}) 
+
+it("<App /> renders without crashing", () => {
+  const wrapper = shallow(<App />)
+})
+
+it("<App /> has 6 Route child components", () => {
+  const wrapper = shallow(<App />)
+})
