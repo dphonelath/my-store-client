8ac292fa9533241c7033751fbb187d84dec167f9
Daniel Phonelath <daniel.phonelath@gmail.com>
2019-08-08 15:19:07 -0600

    refactored tests using describe method

--- src/components/__test__/Navigation.test.js
+++ src/components/__test__/Navigation.test.js
@@ -4,55 +4,61 @@ import Navigation from "../Navigation"
 import * as Auth0 from "../../react-auth0-wrapper"
 import { Link } from "react-router-dom"
 
-it("<Navigation /> renders with 3 Link components", () => {
-  jest.spyOn(Auth0, "useAuth0").mockImplementation(() => {
-    return {
-      isAuthenticated: false
-    }
+describe("<Navigation /> functionality", () => {
+  it("renders with 3 Link components", () => {
+    jest.spyOn(Auth0, "useAuth0").mockImplementation(() => {
+      return {
+        isAuthenticated: false
+      }
+    })
+  
+    const wrapper = shallow(<Navigation />)
+  
+    expect(wrapper.find(Link).length).toBe(3)
+  })
+  
+  it("renders one button when isAuthenticated = false", () => {
+    jest.spyOn(Auth0, "useAuth0").mockImplementation(() => {
+      return{
+        isAuthenticated: false
+      }
+    })
+  
+    const wrapper = shallow(<Navigation />)
+  
+    expect(wrapper.find("button").length).toBe(1)
+  })
+  
+  
+  it("renders 4 Link components when isAuthenticated is true", () => {
+    jest.spyOn(Auth0, "useAuth0").mockImplementation(() => {
+      return{
+        isAuthenticated:true
+      }
+    })
+  
+    const wrapper = shallow(<Navigation />)
+  
+    expect(wrapper.find(Link).length).toBe(4)
+  })
+  
+  it("renders 1 button with text 'Log out' when isAuthenticated = true", () => {
+    jest.spyOn(Auth0, "useAuth0").mockImplementation(() => {
+      return{
+        isAuthenticated:true
+      }
+    })
+  
+    const wrapper = shallow(<Navigation />)
+  
+    expect(wrapper.find("button").length).toBe(1)
+    expect(wrapper.find("button").html()).toMatch("Log out")
   })
 
-  const wrapper = shallow(<Navigation />)
-
-  expect(wrapper.find(Link).length).toBe(3)
-})
-
-it("<Navigation /> renders one button when isAuthenticated = false", () => {
-  jest.spyOn(Auth0, "useAuth0").mockImplementation(() => {
-    return{
-      isAuthenticated: false
-    }
-  })
-
-  const wrapper = shallow(<Navigation />)
-
-  expect(wrapper.find("button").length).toBe(1)
 })
 
 
-it("<Navigation /> renders 4 Link components when isAuthenticated is true", () => {
-  jest.spyOn(Auth0, "useAuth0").mockImplementation(() => {
-    return{
-      isAuthenticated:true
-    }
-  })
 
-  const wrapper = shallow(<Navigation />)
-
-  expect(wrapper.find(Link).length).toBe(4)
-})
-
-it("<Navigation /> renders 1 button with text 'Log out' when isAuthenticated = true", () => {
-  jest.spyOn(Auth0, "useAuth0").mockImplementation(() => {
-    return{
-      isAuthenticated:true
-    }
-  })
-
-  const wrapper = shallow(<Navigation />)
-
-  expect(wrapper.find("button").length).toBe(1)
-  expect(wrapper.find("button").html()).toMatch("Log out")
-})
 
 
 
