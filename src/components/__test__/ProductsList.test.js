bc137bbe620a84b7b22dc6f6fd66fc3305858645
Daniel Phonelath <daniel.phonelath@gmail.com>
2019-08-08 16:07:45 -0600

    tested for componentDidMount in ProductsList.js

--- src/components/__test__/ProductsList.test.js
+++ src/components/__test__/ProductsList.test.js
@@ -7,20 +7,32 @@ import Loader from "../Loader"
 describe("<ProductsList /> functionality", () => {
   let wrapper 
   const spyDidMount = jest.spyOn(ProductsList.prototype, "componentDidMount")
-  
+
   beforeEach(() => {
     wrapper = shallow(<ProductsList />)
   })
 
-  it("calls componentDidMount", () => {
-    expect(spyDidMount).toHaveBeenCalledTimes(1)
+  afterEach(() => {
+    spyDidMount.mockReset()
   })
 
   it("renders without crashing", () => {
     expect(wrapper).toBeTruthy()
   })
 
+  it("calls componentDidMount 1 time", () => {
+    expect(spyDidMount).toHaveBeenCalledTimes(1)
+  })
+
   it("renders a <Loader /> if no data is fetched", () => {
     expect(wrapper.find(Loader).length).toBe(1)
+  })
+
+  it("has an isLoaded property on state that = false", () => {
+    expect(wrapper.state('isLoaded')).toBe(false)
+  })
+
+  it("has an empty products array on state", () => {
+      expect(wrapper.state('products').length).toBe(0)
   })
 })
