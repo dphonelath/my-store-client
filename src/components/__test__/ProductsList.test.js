66f07834b08f043b67b9a2d4344e4eba1c29b0dc
Daniel Phonelath <daniel.phonelath@gmail.com>
2019-08-08 16:35:36 -0600

    simulated a mock rendering of componenetDidMount in ProductsList.test.js

--- src/components/__test__/ProductsList.test.js
+++ src/components/__test__/ProductsList.test.js
@@ -5,34 +5,67 @@ import ProductsList from "../ProductsList"
 import Loader from "../Loader"
 
 describe("<ProductsList /> functionality", () => {
-  let wrapper 
-  const spyDidMount = jest.spyOn(ProductsList.prototype, "componentDidMount")
+  describe("Initial component render", () => {
+    let wrapper 
+    const spyDidMount = jest.spyOn(ProductsList.prototype, "componentDidMount")
 
-  beforeEach(() => {
-    wrapper = shallow(<ProductsList />)
-  })
+    beforeEach(() => {
+      wrapper = shallow(<ProductsList />)
+    })
 
-  afterEach(() => {
-    spyDidMount.mockReset()
-  })
+    afterEach(() => {
+      spyDidMount.mockReset()
+    })
 
-  it("renders without crashing", () => {
-    expect(wrapper).toBeTruthy()
-  })
+    afterAll(() => {
+      spyDidMount.mockRestore()
+    })
 
-  it("calls componentDidMount 1 time", () => {
-    expect(spyDidMount).toHaveBeenCalledTimes(1)
-  })
+    it("renders without crashing", () => {
+      expect(wrapper).toBeTruthy()
+    })
 
-  it("renders a <Loader /> if no data is fetched", () => {
-    expect(wrapper.find(Loader).length).toBe(1)
-  })
+    it("calls componentDidMount 1 time", () => {
+      expect(spyDidMount).toHaveBeenCalledTimes(1)
+    })
 
-  it("has an isLoaded property on state that = false", () => {
-    expect(wrapper.state('isLoaded')).toBe(false)
-  })
+    it("renders a <Loader /> if no data is fetched", () => {
+      expect(wrapper.find(Loader).length).toBe(1)
+    })
 
-  it("has an empty products array on state", () => {
+    it("has an isLoaded property on state that = false", () => {
+      expect(wrapper.state('isLoaded')).toBe(false)
+    })
+
+    it("has an empty products array on state", () => {
       expect(wrapper.state('products').length).toBe(0)
+    })
+
+    it("renders a header", () => {
+      expect(wrapper.find("header").length).toBe(1)
+    })
   })
+  
+  describe("render after data is fetched", () => {
+    let wrapper 
+    window.fetch = jest.fn().mockImplementation(() => {
+      return Promise.resolve({
+        status: 200,
+        json: () => {
+          return Promise.resolve({
+            products: [{ id: 1, name: "thing"}, { id: 2, name: "thing2"}]
+          })
+        }
+      })
+    })
+
+    beforeEach(() => {
+      wrapper = shallow(<ProductsList />)
+    })
+
+    it("renders a <Loader /> if no data is fetched", () => {
+      expect(wrapper.find(Loader).length).toBe(1)
+    })
+  })
+
 })
